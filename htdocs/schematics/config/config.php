b3a4f14060a60ac3b73912d9b357fe798e2bd453
godinlu <luc.villaroux@orange.fr>
2024-09-06 09:49:11 +0200

    update version

--- htdocs/schematics/config/config.php
+++ htdocs/schematics/config/config.php
@@ -1,5 +1,5 @@
 <?php 
-const VERSION = "2.0.22";
+const VERSION = "2.0.221";
 //cette constante une fois indiqué à true va forcer le rechargement des fichiers javaScript pour
 //éviter qu'ils soit mis en cache
 const DISABLED_CACHE = false;
