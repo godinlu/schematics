4291c7d27d2e4ac73d67235f7707a72ac8fdf8d2
godinlu <luc.villaroux@orange.fr>
2025-12-02 15:36:15 +0100

    first step in the new devis

--- htdocs/schematics/api/get_categorie.php
+++ htdocs/schematics/api/get_categorie.php
@@ -3,11 +3,15 @@ require_once '../models/Model.php';
 
 class CategorieManager extends Model{
     public function get_categories(){
-        return $this->getAll('Categorie','Article');
+        return $this->select("SELECT * FROM Categorie");
     }
 }
 
 $categorie_manager = new CategorieManager;
+
+// $data = array_values($categorie_manager->get_categories());
+// var_dump($data);
+// exit;
 header('Content-Type: application/json');
 echo json_encode($categorie_manager->get_categories());
 
--- htdocs/schematics/controllers/client/ControllerDevis.php
+++ htdocs/schematics/controllers/client/ControllerDevis.php
@@ -3,12 +3,27 @@ require_once ('views/View.php');
 require_once ('models/ArticleManager.php');
 require_once ('models/DataForm.php');
 
-//use PhpOffice\PhpSpreadsheet\IOFactory;
+class CategorieManager extends Model{
+    public function get_categories(){
+        return $this->select("SELECT * FROM Categorie");
+    }
+}
+
+class ArticleCategorieManager extends Model{
+    public function get_article_categories(){
+        return $this->select("SELECT * FROM ArticleCategorie");
+    }
+}
+
+class TarifManager extends Model{
+    public function get_articles(){
+        return $this->select("SELECT * FROM Tarif");
+    }
+}
 
 class ControllerDevis
 {
     private $_view;
-    private $_articleManager;
 
     public function __construct($url)
     {
@@ -29,10 +44,15 @@ class ControllerDevis
         header('Location: formulaire');
         exit;
       }
-      $data_importer = new DataImporter($formulaire);
+      $categorie_manager = new CategorieManager;
+      $article_categorie_manager = new ArticleCategorieManager;
+      $tarif_manager = new TarifManager;
 
       $this->_view = new View('Devis');
       $this->_view->generate(array(
+        "categories" => $categorie_manager->get_categories(),
+        "article_categories" => $article_categorie_manager->get_article_categories(),
+        "articles" => $tarif_manager->get_articles()
       ));
     }
 }
