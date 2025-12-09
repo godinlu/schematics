6416b346f8cf7712260e115360893b6bc302f8de
godinlu <luc.villaroux@orange.fr>
2025-12-09 10:43:57 +0100

    complete change on the interface adapt to the new logo

--- htdocs/schematics/views/client/viewSchema_exe.php
+++ htdocs/schematics/views/client/viewSchema_exe.php
@@ -8,11 +8,11 @@
     <img src="../api/getSchema.php?image=Etiquetage" id="Etiquetage" alt="Etiquetage" class="hidden" >
 
   </div>
-  <div id="sideBar">
+  <aside>
     <button type="button" id="btn_toggle_etiquetage">étiquetage module<i class="fa-regular fa-square"></i></button>
     <button type="button" id="btn_download_png">télécharger (png)<i class="fa-regular fa-file-image"></i></button>
     <button type="button" id="btn_download_pdf">télécharger (pdf)<i class="fa-regular fa-file-pdf"></i></button>
-  </div>     
+  </aside>  
 </div>
 <script>
   const formulaire = <?=json_encode($formulaire)?>;
