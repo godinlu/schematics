1afbc42bcb073f5d600261e0f27d86abae1df001
godinlu <luc.villaroux@orange.fr>
2025-12-11 14:46:55 +0100

    remove all version of devis2 or else

--- htdocs/schematics/public/svelte/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Devis from './devis/Devis.svelte';
-	export let title;
-</script>
-
-{#if (title == "Devis2")}
-	<Devis default_articles={window.global_vars["default_articles"]}/>
-{/if}
-
--- htdocs/schematics/public/svelte/src/devis_new/Category.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import { Category } from "./back/Categories";
-    /**
-     * @type {Category} category
-     */
-    export let category;
-</script>
-<h1>{category.name}</h1>
